2778165fbc1368aeec2596cee47aff1df4982cbf
Florian Decker <florian@deckerhome.at>
2015-11-24 21:04:26 +0100

    final touches

--- testseite/password.js
+++ testseite/password.js
@@ -1,8 +1,9 @@
 var pass_entered;
 var enterpassword = "arbeitsrechtsforum"
 
-
-while (pass_entered != enterpassword)
-{
 	pass_entered = prompt("Bitte geben sie ein Passwort ein: ");
+
+if (pass_entered != enterpassword)
+{
+	window.location = "http://forumarbeitsrecht.at"; 
 }
