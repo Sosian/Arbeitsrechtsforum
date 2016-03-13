70598875979f81731d6db936a62158a65f228d39
Florian Decker <florian@deckerhome.at>
2016-03-13 16:55:53 +0100

    only new side, remove password protection

--- testseite/password.js
+++
@@ -1,9 +0,0 @@
-var pass_entered;
-var enterpassword = "arbeitsrechtsforum"
-
-	pass_entered = prompt("Bitte geben sie ein Passwort ein: ");
-
-if (pass_entered != enterpassword)
-{
-	window.location = "http://forumarbeitsrecht.at"; 
-}
